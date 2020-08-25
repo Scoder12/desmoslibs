d92633c4de48a01328b218f675ec62b3768e025c
repl.it user <replituser@example.com>
2020-08-25 18:31:11 +0000

    Add assign function to standard library

--- dscript/__init__.py
+++ dscript/__init__.py
@@ -26,8 +26,12 @@ STANDARD_LIBRARY = {
         r"s_{lice}\left(a,s_{tart},e_{nd}\right)=s_{liceb}\left(a,\left\{s_{tart}<0:\operatorname{length}\left(a\right)+s_{tart}+1,s_{tart}\right\},\left\{e_{nd}<0:\operatorname{length}\left(a\right)+e_{nd}+1,e_{nd}\right\}\right)",
     ],
     "fill": [
-      r"f_{illa}\left(r_{angefill},v_{fill}\right)=\sum_{n=r_{angefill}}^{r_{angefill}}v_{fill}",
-      r"f_{ill}\left(l_{enfill},v_{fill}\right)=f_{illa}\left(\left[1,...l_{enfill}\right],v_{fill}\right)"
+        r"f_{illa}\left(r_{angefill},v_{fill}\right)=\sum_{n=r_{angefill}}^{r_{angefill}}v_{fill}",
+        r"f_{ill}\left(l_{enfill},v_{fill}\right)=f_{illa}\left(\left[1,...l_{enfill}\right],v_{fill}\right)"
+    ],
+    "assign": [
+        r"a_{ssigna}\left(l_{asn},i_{asn},v_{asn},t_{asn}\right)=\sum_{n=t_{asn}}^{t_{asn}}\left\{i_{asn}=n:v_{asn},l_{asn}\left[n\right]\right\}"
+        r"a_{ssign}\left(l_{asn},i_{asn},v_{asn}\right)=a_{ssigna}\left(l_{asn},i_{asn},v_{asn},\left[1,...,\operatorname{length}\left(l_{asn}\right)\right]\right)",
     ]
 }
 
